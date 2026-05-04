55dec3ecc019dc4c8820fc051884518fc00565a6
Samrah Khanzada <samrahsajidkhanzada@gmail.com>
2026-05-04 14:35:47 +0500

    Update allowed origins for CORS configuration

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(cors({
   // Multiple origins allowed (ENV variable + Hardcoded for safety)
   origin: [
     process.env.FRONTEND_URL, 
-    'https://blog-auth-frontend.vercel.app'
+    'https://blog-auth-frontend-three.vercel.app/'
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
